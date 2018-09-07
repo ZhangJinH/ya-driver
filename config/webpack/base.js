e5c1edc6b65170af06800937fe1c966fa5666f23
q13 <qsimon13@gmail.com>
2018-09-07 19:10:22 +0800

    Add some webpack configs

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,6 +1,22 @@
 /**
  * The base webpack config
  */
-module.exports = function () {
+const path = require('path');
 
+module.exports = function (options) {
+  const {
+    projectPath
+  } = options;
+  const main = path.resolve(projectPath, './src/index.js');
+  return {
+    entry: {
+      main
+    },
+    resolve: {
+      alias: {
+        '+': path.resolve(projectPath, './node_modules/ya-spa-vue/ya'),
+        '@': path.resolve(projectPath, './src')
+      }
+    }
+  };
 };
